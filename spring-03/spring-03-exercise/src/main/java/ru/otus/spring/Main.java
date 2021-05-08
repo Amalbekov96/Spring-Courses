a8265f4e70eb549c84e55d812b46dd264cb38cb1
Amalbekov96 <Amalbekov312@gmail.com>
2021-05-08 07:22:44 -0400

    Have worked on LoggingAspect.java file. Figure out correct annotations for creating an AOP.

--- spring-03/spring-03-exercise/src/main/java/ru/otus/spring/Main.java
+++ spring-03/spring-03-exercise/src/main/java/ru/otus/spring/Main.java
@@ -3,7 +3,7 @@ package ru.otus.spring;
 import org.springframework.context.annotation.*;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
-
+@EnableAspectJAutoProxy
 @Configuration
 @ComponentScan
 public class Main {
@@ -13,7 +13,6 @@ public class Main {
                 new AnnotationConfigApplicationContext(Main.class);
 
         PersonService service = context.getBean(PersonService.class);
-
         Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
--- spring-03/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
+++ spring-03/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
@@ -1,9 +1,17 @@
 package ru.otus.spring.logging;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class LoggingAspect {
 
+    @Around("execution(* ru.otus.dao.*.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("Вызов метода : " + joinPoint.getSignature().getName());
     }
